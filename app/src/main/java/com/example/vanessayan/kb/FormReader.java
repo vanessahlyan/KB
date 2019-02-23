26bd7633296815174447b6b040565a522f8e7285
Vanessa Yan <vanessahlyan@gmail.com>
2019-02-22 20:42:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/vanessayan/kb/FormReader.java
@@ -0,0 +1,71 @@
+package com.example.vanessayan.kb;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FormReader {
+    public static final int UTF_CONVERSION = 8;
+    public static final int VERSION = 1;
+    /* This function assumes that the form builder app outputs a csv file with lines reading "fieldname, type, max"
+     * The form builder app also asks the user for the size of their nfc tag.
+     */
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public JSONObject ReadCSV(String csvFile) {
+        ArrayList<ArrayList> store = new ArrayList<>();
+        JSONObject form = new JSONObject();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            int totalBits = 0;
+            String line;
+            while ((line = br.readLine()) != null) {
+                ArrayList field = new ArrayList();
+                // use comma as separator
+                String[] split = line.split(","); // 3 tokens
+
+                int max = Integer.parseInt(split[2]);
+                int size = 0;
+
+                if (split[1].equals("text")) {
+                    // utf-8 encoding
+                    size = UTF_CONVERSION * max;
+                    totalBits += size;
+                } else if (split[1].equals("integer")) {
+                    //log base 2 of pos int
+                    int count = 0;
+                    while (max > 0) {
+                        count++;
+                        max /= 2;
+                    }
+                    size = count;
+                    totalBits += size;
+                } else if (split[1].equals("boolean")) {
+                    size = 1;
+                    totalBits += size;
+                }
+                field.add(split[0]);
+                field.add(split[1]);
+                field.add(Integer.toString(size));
+                store.add(field);
+
+                form.put("metadata", new JSONObject().put("version", VERSION).put("info", Integer.toString(totalBits)));
+                for (int i = 0; i < store.size(); i++) {
+                    form.put((String) store.get(i).get(0), new JSONObject().put("type", store.get(i).get(1)).put("size", store.get(i).get(2)));
+                }
+            }
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return form;
+    }
+}
---
+++ app/src/main/java/com/example/vanessayan/kb/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.vanessayan.kb;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.json.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FormReader fr = new FormReader();
+        JSONObject fields = fr.ReadCSV("E:/Documents/KhushiBaby/sampleResponses.csv");
+
+        try (FileWriter file = new FileWriter("E:/Documents/KhushiBaby/sample.txt")) {
+            file.write(fields.toString());
+            System.out.println("Successfully Copied JSON Object to File...");
+            System.out.println("JSON Object: " + fields.toString() + "\n");
+        }
+        catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
